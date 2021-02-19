f70dbea272e96e6b72308ebe3656b785a24b4d25
Tyler Cook <tyler.cook@self.inc>
2021-02-19 17:21:35 -0600

    Added RN library for drawing. Added some of the basic capability to draw and our custom controls

--- App.js
+++ App.js
@@ -1,114 +1,76 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { useRef, useState } from 'react'
+import { SafeAreaView, StyleSheet, View, Text, Button } from 'react-native'
+import { SketchCanvas } from '@terrylinla/react-native-sketch-canvas'
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+const App = () => {
+  const sketchRef = useRef(null)
+  const [strokeWidth, setStrokeWidth] = useState(1)
+  const [strokeColor, setStrokeColor] = useState('#ff0000')
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+    <SafeAreaView style={{ flex: 1, padding: 10, position: 'relative' }}>
+      <View style={{ backgroundColor: 'grey', flexDirection: 'column' }}>
+        <View style={{ flexDirection: 'row' }}>
+          <Button
+            title='Erase'
+            onPress={() => {
+              sketchRef.current.clear()
+            }}
+          />
+          <Button
+            title='Pencil'
+            onPress={() => {
+              setStrokeWidth(1)
+            }}
+          />
+          <Button
+            title='Marker'
+            onPress={() => {
+              setStrokeWidth(10)
+            }}
+          />
+          <Button
+            title='Save'
+            onPress={() => {
+              sketchRef.current.save('jpg', false, 'filae', 'test2', true, true, true)
+            }}
+          />
+        </View>
+        <View style={{ flexDirection: 'row' }}>
+          <Button
+            title='Red'
+            onPress={() => {
+              setStrokeColor('#ff0000')
+            }}
+          />
+          <Button
+            title='Orange'
+            onPress={() => {
+              setStrokeColor('#ff8300')
+            }}
+          />
+          <Button
+            title='Yellow'
+            onPress={() => {
+              setStrokeColor('#fff500')
+            }}
+          />
+        </View>
+      </View>
+      <SketchCanvas
+        ref={sketchRef}
+        style={{ flex: 1, backgroundColor: 'white' }}
+        strokeColor={strokeColor}
+        strokeWidth={strokeWidth}
+        onSketchSaved={(success, filePath) => {
+          console.log('stat', success)
+          console.log('path', filePath)
+        }}
+      />
+    </SafeAreaView>
+  )
+}
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+const styles = StyleSheet.create({})
 
-export default App;
+export default App
